531d7a43ca033cee02733899282826b1afd14a5c
Sheraz Manzoor <126092064+sheraz4196@users.noreply.github.com>
2023-11-20 17:14:00 +0500

    Update App.jsx
    Adding Footer to Game

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
-import Game from '../Game';
-import Header from '../Header';
-
+import Game from "../Game";
+import Header from "../Header";
+import Footer from "../footer/footer";
 function App() {
   return (
     <div className="wrapper">
       <Header />
-
       <div className="game-wrapper">
         <Game />
       </div>
+      <Footer />
     </div>
   );
 }
