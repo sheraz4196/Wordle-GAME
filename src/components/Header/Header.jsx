9b3e4b5c9f9f047a652f29282674d08b6ea2b4fe
Sheraz Manzoor <126092064+sheraz4196@users.noreply.github.com>
2023-11-20 17:13:07 +0500

    Update Header.jsx
    Header optimized

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import Menu from "../Menu/Menu";
 function Header() {
   return (
-    <header>
-      <h1>Word Game</h1>
+    <header className="header">
+      <div className="game-info">
+        <img
+          src="../../public/favicon.09ab7678.png"
+          alt="Game Icon"
+          className="game-icon"
+        />
+        <h1>Word Game</h1>
+      </div>
+      <Menu />
     </header>
   );
 }
